bcb8366a989ea612551d65c9923dca0d8641cd42
Mawls <lanamaulana2002@gmail.com>
2022-08-28 21:40:43 +0700

    membuat program pengelolaan penumpang

---
+++ Javascript/Array/Program pengelolaan penumpang/script.js
@@ -0,0 +1,39 @@
+let penumpang = ["maul", undefined, "abdurahman"];
+
+// function tambah penumpang
+const tambahPenumpang = (namaPenumpang, penumpang) => {
+  // jika kendaraan kosong
+  if (penumpang.length == 0) {
+    // tambah penumpang diawal array
+    penumpang.push(namaPenumpang);
+    // kembalikan isi array & keluar dari function
+    return penumpang;
+  } else {
+    // telusuri kursi dari awal
+    for (let i = 0; i < penumpang.length; i++) {
+      // jika ada kursi kosong
+      if (penumpang[i] == undefined) {
+        // tambah penumpang di kursi tersebut
+        penumpang[i] = namaPenumpang;
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+      }
+
+      // jika seluruh kursi terisi
+      else if (i == penumpang.length - 1) {
+        // tambah penumpang di akhir array
+        penumpang.push(namaPenumpang);
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+      }
+
+      // jika sudah ada nama yang sama
+      else if (penumpang[i] == namaPenumpang) {
+        // tampilkan pesan kesalahannya
+        console.log(`${namaPenumpang} sudah ada di dalam kendaraan`);
+        // kembalikan isi array & keluar dari function
+        return penumpang;
+      }
+    }
+  }
+};
