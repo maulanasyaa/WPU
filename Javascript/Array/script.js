f7bf2b0c84fbfa4e2b20b3f708c3406d556262f1
Mawls <lanamaulana2002@gmail.com>
2022-08-28 08:54:38 +0700

    add html and js file for learn Array

---
+++ Javascript/Array/script.js
@@ -0,0 +1,22 @@
+// Manipulasi Array
+
+// 1. Join (merubah isi array menjadi string dan menampilkan nya)
+
+let arr = ["maulana", "syarip", "abdurahman"];
+console.log(arr.join(" "));
+
+// 2. Push & pop (menambahkan dan mengurangi elemen pada akhir array)
+
+arr.push(7, 5, 2);
+console.log(arr);
+
+arr.pop();
+console.log(arr);
+
+// 3.  Unshift & shift (menambahkan dan mengurangi elemen pada awal array)
+
+arr.unshift("halo", "nama saya");
+console.log(arr);
+
+arr.shift();
+console.log(arr);
