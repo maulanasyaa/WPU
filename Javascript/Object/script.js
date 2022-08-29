9ecf516d61f4301fb1403599ddd698cf47d887c1
Mawls <lanamaulana2002@gmail.com>
2022-08-29 08:44:21 +0700

    add a few ways to make an object

--- Javascript/Object/script.js
+++ Javascript/Object/script.js
@@ -1,6 +1,10 @@
-const person = {
+// Membuat object
+
+// Object literal
+const personal = {
   nama: "Maul",
   umur: 20,
+  email: "lanamaulana2002@gmail.com",
   alamat: {
     kelurahan: "karawang kulon",
     kecamatan: "karawang barat",
@@ -8,3 +12,26 @@ const person = {
   },
   goal: "being productive",
 };
+
+// function declaration
+const buatObjectPerson = (nama, umur, kota) => {
+  let person = {};
+  person.nama = nama;
+  person.umur = umur;
+  person.kota = kota;
+  return person;
+};
+
+const person1 = buatObjectPerson("maul", 20, "karawang");
+
+// Constructor
+function People(nama, umur, kota) {
+  // variabel this = {}
+  this.nama = nama;
+  this.umur = umur;
+  this.kota = kota;
+  //   return this
+}
+
+const people1 = new People("maul", 20, "karawang");
+const people2 = new People("syarip", 20, "karawang");
