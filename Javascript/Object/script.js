d5d77cbe070751b356e1f9e397fde3391e3bd39c
Mawls <lanamaulana2002@gmail.com>
2022-08-29 08:28:02 +0700

    make a first object

---
+++ Javascript/Object/script.js
@@ -0,0 +1,10 @@
+const person = {
+  nama: "Maul",
+  umur: 20,
+  alamat: {
+    kelurahan: "karawang kulon",
+    kecamatan: "karawang barat",
+    kabupaten: "karawang",
+  },
+  goal: "being productive",
+};
