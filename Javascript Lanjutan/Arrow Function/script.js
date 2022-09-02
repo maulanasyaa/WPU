7a52322d12f1aaff9fd766eaa1f0b9b48d72d5ab
Mawls <lanamaulana2002@gmail.com>
2022-09-02 09:22:04 +0700

    create this concept in arrow function and create css file

--- Javascript Lanjutan/Arrow Function/script.js
+++ Javascript Lanjutan/Arrow Function/script.js
@@ -30,7 +30,7 @@
 // console.log(tampilNama());
 
 // contoh
-let mahasiswa = ["Maulana", "Syarip", "Abdurahman"];
+// let mahasiswa = ["Maulana", "Syarip", "Abdurahman"];
 
 // let jumlahHuruf = mahasiswa.map(function (nama) {
 //   return nama.length;
@@ -40,5 +40,60 @@ let mahasiswa = ["Maulana", "Syarip", "Abdurahman"];
 // let jumlahHuruf = mahasiswa.map((nama) => nama.length);
 // console.log(jumlahHuruf);
 
-let jumlahHuruf = mahasiswa.map((nama) => ({ nama, jmlHuruf: nama.length }));
-console.table(jumlahHuruf);
+// let jumlahHuruf = mahasiswa.map((nama) => ({ nama, jmlHuruf: nama.length }));
+// console.table(jumlahHuruf);
+
+// Konsep this pada arrow function
+
+// Constructor Function
+// const Mahasiswa = function () {
+//   this.nama = "Maul";
+//   this.umur = 20;
+//   this.sayHello = function () {
+//     console.log(`Halo nama saya ${this.nama}, dan saya berumur ${this.umur} tahun.`);
+//   };
+// };
+
+// const maul = new Mahasiswa();
+
+// Arrow Function
+// const Mahasiswa = function () {
+//   this.nama = "Maul";
+//   this.umur = 20;
+//   this.sayHello = () => {
+//     console.log(`Halo nama saya ${this.nama}, dan saya berumur ${this.umur} tahun.`);
+//   };
+// };
+
+// const maul = new Mahasiswa();
+
+// const Mahasiswa = function () {
+//   this.nama = "Maul";
+//   this.umur = 20;
+//   this.sayHello = function () {
+//     console.log(`Halo nama saya ${this.nama}, dan saya berumur ${this.umur} tahun.`);
+//   };
+
+//   setInterval(() => {
+//     console.log(this.umur++);
+//   }, 5000);
+// };
+
+// const maul = new Mahasiswa();
+
+// real case
+const box = document.querySelector(".box");
+box.addEventListener("click", function () {
+  let satu = "size";
+  let dua = "caption";
+
+  if (this.classList.contains(satu)) {
+    [satu, dua] = [dua, satu];
+  }
+
+  this.classList.toggle(satu);
+  setTimeout(() => {
+    this.classList.toggle(dua);
+  }, 600);
+  //   setTimeout menggunakan arrow function karena ingin menggunakan this = box
+});
